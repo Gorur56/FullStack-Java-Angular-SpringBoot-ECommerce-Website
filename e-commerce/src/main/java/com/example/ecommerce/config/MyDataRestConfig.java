1dbae57a2cef52be0b91f5cdbdae6aebaceb0640
Gorur56 <sevim.gorur01@gmail.com>
2024-05-16 13:20:08 +0300

    ECommerce Project Salt okunur yapma. GET dışında Diğer Http methodlarını devredışı bırakma

---
+++ e-commerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,40 @@
+package com.example.ecommerce.config;
+
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        //RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        //Http methodlarını devre dışı bırakıyoruz: PUT, POST, and DELETE
+        //Yalnızca GET çalışacak ve salt okunur olacaktır.
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        /* Postman 'da aşağıdaki kodu POST method 'u ile gönderdiğmizde;
+            {
+                 "test":"data"
+            }
+
+            405Method Not Allowed
+         */
+    }
+}
