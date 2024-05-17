365a83219dc0c15de8ba95ea8d388876049286fa
Gorur56 <sevim.gorur01@gmail.com>
2024-05-17 21:12:09 +0300

    Angular create product service ts file

--- 03-FRONTEND/angular-ecommerce/src/app/app.component.ts
+++ 03-FRONTEND/angular-ecommerce/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent], //Anguler ı kurarken ProductListComponent eklemiz gerekiyor. Yoksa <app-product-list></app-product-list> hata veriyor.
+  imports: [RouterOutlet, ProductListComponent, HttpClientModule], //Anguler ı kurarken ProductListComponent eklemiz gerekiyor. Yoksa <app-product-list></app-product-list> hata veriyor.
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'angular-ecommerce';
--- 03-FRONTEND/angular-ecommerce/src/app/services/product.service.ts
+++ 03-FRONTEND/angular-ecommerce/src/app/services/product.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,19 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   //CREATE ANGULAR SERVICE: ng generate service services/product
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/api/products"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<Getresponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
+}
+
+interface Getresponse {
+  _embedded: {
+    products: Product[];
+  }
 }
